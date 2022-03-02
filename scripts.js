3454127036f1f96ce5999dbd85627dfdc56c06dc
lerdr <lerdr@protonmail.com>
2022-03-02 12:39:57 +0100

    Basic interaction

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@
 // add listeners on buttons ✔️
 // create mock list in the background ✔️
 // append & reset elements via btns ✔️
-// "title" is text field, preselected on refresh 💬
+// "title" is text field, preselected on refresh ✔️
+// pressing "enter" auto-clicks btn1 & clears title + autofocus ✔️
+// pressing "shift + enter" logs "Extended functionalities to come" ✔️
 
 const cardTitle = document.getElementById("card-title");
 const cardDescription = document.getElementById("card-description");
@@ -12,11 +14,19 @@ const btn2 = document.getElementById("btn-2");
 
 const bgList = document.getElementById("bg-list");
 
+const keys = {};
+onkeydown = onkeyup = function (key) {
+  keys[key.key] = key.type == "keydown";
+
+  if (keys["Enter"] && !keys["Shift"]) appendTitle();
+  if (keys["Enter"] && keys["Shift"])
+    console.log("Extended functionalities to come");
+};
+
 function btnClick() {
   switch (this.dataset.function) {
     case "append":
-      const content = cardTitle.innerHTML;
-      bgList.innerHTML += `<li>${content}</li>`;
+      appendTitle();
       break;
     case "reset":
       bgList.innerHTML = "";
@@ -25,6 +35,14 @@ function btnClick() {
       console.log("No function detected");
       break;
   }
+}
+
+function appendTitle() {
+  const content = cardTitle.value.trim();
+  if (content === "") return;
+  bgList.innerHTML += `<li>${content}</li>`;
+  cardTitle.value = "";
+  cardTitle.focus();
 }
 
 btn1.addEventListener("click", btnClick);
