ebda5cb37b71215273cb69ed54e1e69f28309603
lerdr <lerdr@protonmail.com>
2022-03-02 13:14:20 +0100

    Custom description, items array

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
-// DONE: ✔️ - WIP: 💬 - ERROR: ❌
+// DONE: ✔️ - WIP: 💬 - ERROR: ❌ - ON HOLD: 🔜 - BUG: 🐛
 // add listeners on buttons ✔️
 // create mock list in the background ✔️
 // append & reset elements via btns ✔️
 // "title" is text field, preselected on refresh ✔️
 // pressing "enter" auto-clicks btn1 & clears title + autofocus ✔️
 // pressing "shift + enter" logs "Extended functionalities to come" ✔️
+// title -> description -> enter; tabindex & custom content ✔️
+// create to array instead of list, just log ✔️
+// draw background from array, not list ✔️
+// draw whole background from array, not just append 💬
+
+// how do we edit elements? 🔜
+// how do we empty the list, if the "shift+enter" won't be reset? 🔜
+
+// fix: empty title - tab to description - write - enter: no trigger 🐛
+
+const items = [];
 
 const cardTitle = document.getElementById("card-title");
 const cardDescription = document.getElementById("card-description");
@@ -29,7 +40,12 @@ function btnClick() {
       appendTitle();
       break;
     case "reset":
+      cardTitle.value = "";
+      cardDescription.value = "";
+      cardTitle.focus();
       bgList.innerHTML = "";
+      items.splice(0, items.length);
+      console.log(items);
       break;
     default:
       console.log("No function detected");
@@ -38,11 +54,20 @@ function btnClick() {
 }
 
 function appendTitle() {
-  const content = cardTitle.value.trim();
-  if (content === "") return;
-  bgList.innerHTML += `<li>${content}</li>`;
+  const titleValue = cardTitle.value.trim();
+  const descriptionValue = cardDescription.value.trim();
+  if (titleValue === "") return;
+  items.push({ title: titleValue, description: descriptionValue });
+
   cardTitle.value = "";
+  cardDescription.value = "";
   cardTitle.focus();
+
+  drawList(titleValue, descriptionValue);
+}
+
+function drawList(title, description) {
+  bgList.innerHTML += `<li><strong>${title}</strong></li><br>${description}`;
 }
 
 btn1.addEventListener("click", btnClick);
